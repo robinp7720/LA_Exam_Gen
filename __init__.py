c7959c32ada57ca4bf2419c3813f62e7c5e99418
EricHier <developer.erichier@gmail.com>
2020-09-06 12:24:48 +0200

    fixed typos and added gitignore

--- __init__.py
+++ __init__.py
@@ -51,7 +51,7 @@ if __name__ == '__main__':
 
     print('Generating questions')
 
-    #generator.addQuestion(question_types.char_polynomial.generate_question())
+    generator.addQuestion(question_types.char_polynomial.generate_question())
     generator.addQuestion(question_types.determinant.generate_question())
     generator.addQuestion(question_types.dot_product.generate_question())
     generator.addQuestion(question_types.determinant_with_variable.generate_question())
